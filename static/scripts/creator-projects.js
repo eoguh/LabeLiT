375b074cdaac43dc685d8d708ace0f5c3d50de9a
Cosmos Adedero <cosmosjunior0495@gmail.com>
2023-05-09 17:18:18 +0100

    Dynamic icons

--- static/scripts/creator-projects.js
+++ static/scripts/creator-projects.js
@@ -12,3 +12,22 @@ document.getElementById("myNav").style.width = "100%";
 popUpExit.addEventListener('click', () => {
 document.getElementById("myNav").style.width = "0%";
 });
+
+const newProjectNameForm = document.getElementById("edit-project-name-form");
+
+function editProjectName() {
+    
+    newProjectNameForm.style.display = "flex";
+};
+
+function closeEditNameForm() {
+    newProjectNameForm.style.display = "none";
+};
+
+function updateProjectName() {
+    var newProjectName = document.getElementById("input-new-project-name");
+    var oldProjectName = document.getElementById("project-name");
+
+    // Update the project name with the new input value
+    oldProjectName.textContent = newProjectName.value;
+};
